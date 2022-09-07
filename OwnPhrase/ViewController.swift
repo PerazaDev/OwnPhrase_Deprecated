8aad596f3f726bfd23fc0bc412a8f82cd625dc12
DanielSP99 <daniel.sanchez@ingenierosmafur.com>
2022-09-06 21:28:36 -0500

    Initial Commit

---
+++ OwnPhrase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OwnPhrase
+//
+//  Created by Daniel Sanchez Peraza on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
